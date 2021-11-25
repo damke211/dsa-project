c7e148720e6f901757e8649671cb51a97cc9343b
Damir Halilovic <damke211@gmail.com>
2021-11-25 20:51:49 +0100

    Added additional icons and labels

--- frontend/dsa-fe/src/constants.js
+++ frontend/dsa-fe/src/constants.js
@@ -1,10 +1,28 @@
-export const Light = {
-    on: "Light is on",
-    off: "Light is off"
+export const Labels = {
+    light:{
+        on:"Light is on!",
+        off:"Light is off"
+    },
+    motion:{
+        on:"Presence detected!",
+        off:"No motion detected"
+    },
+    ultrasonic:{
+        close:"Object is close!",
+        far:"No objects nearby"
+    },
+    alarm:{
+        on:"Alarm is on!",
+        off:"Aalarm is off"
+    },
+    door:{
+        opening:"Opening door",
+        closing:"Closing door",
+        open:"Door is open",
+        closed:"Door is closed"
+    },
+    ldr:{
+        on:"Its dark outside",
+        off:"There is still daylight"
+    }
 }
-
-export const Motion = {
-    detected:"Detected moving object!",
-    notDetected:"There is no movement"
-}
-
